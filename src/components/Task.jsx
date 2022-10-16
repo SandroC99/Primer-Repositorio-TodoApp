27a23e0ef753b0325a833470b5261c54631f0451
Sandro <sandrosc99@hotmail.com>
2022-10-16 15:43:23 -0500

    Commit_4: Version 1.0 del Todo

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import '../css/Task.css';
 
-export const Task = () => {
+export const Task = ({ id, complete, task, updateFun, deleteFun }) => {
 	return (
-		<>
-			<li className='task'>
-				<span>Tarea Random</span>
-				<button>Eliminar</button>
-			</li>
-		</>
+		<li
+			className={`task ${complete ? 'complete' : ''}`.trim()}
+			onClick={() => updateFun(id)}
+		>
+			<span>{task}</span>
+			<button
+				onClick={(e) => {
+					e.stopPropagation();
+					deleteFun(id);
+				}}
+			>
+				Eliminar
+			</button>
+		</li>
 	);
 };
--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,18 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Task } from './Task';
 import '../css/TodoForm.css';
+import { v4 as uuidv4 } from 'uuid';
 
 export const TodoForm = () => {
+	const [input, setInput] = useState('');
+	const [list, setList] = useState([]);
+
+	const HANDLE_CHANGE = (e) => {
+		setInput(e.target.value);
+	};
+	const ADD_TASK = (e) => {
+		e.preventDefault();
+		const newTask = {
+			id: uuidv4(),
+			text: input,
+			complete: false,
+		};
+		setList([...list, newTask]);
+	};
+
+	const UPDATE_FUN = (id) => {
+		console.log('update');
+		let updList = list.map((task) => {
+			if (task.id === id) {
+				task.complete = !task.complete;
+			}
+			return task;
+		});
+		setList(updList);
+	};
+	const DELETE_FUN = (id) => {
+		console.log('delete');
+		let newList = list.filter((task) => task.id !== id);
+		setList(newList);
+	};
+
 	return (
 		<>
 			<div className='todo_container'>
 				<form action='' className='todo_form'>
-					<input type='text' />
-					<button>Agregar</button>
+					<input type='text' onChange={HANDLE_CHANGE} />
+					<button onClick={ADD_TASK}>Agregar</button>
 				</form>
 				<div className='todo_list'>
 					<ul className='list'>
-						<Task />
+						{list.map((task) => {
+							return (
+								<Task
+									id={task.id}
+									key={task.id}
+									complete={task.complete}
+									task={task.text}
+									updateFun={UPDATE_FUN}
+									deleteFun={DELETE_FUN}
+								/>
+							);
+						})}
 					</ul>
 				</div>
 			</div>
